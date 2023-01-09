f40b8ab033a880ffc3564a23129dda14fefd5f85
Igor Josić <igor-josic@hotmail.com>
2023-01-09 21:47:17 +0100

    End of section 2

---
+++ NzWalksAPI/NzWalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalksAPI.Models.Domain;
+
+namespace NzWalksAPI.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base (options) 
+        {
+
+
+        }
+
+
+
+        /// <summary>
+        /// Tabele imena
+        /// </summary>
+        /// 
+        public DbSet<Region> Regions { get; set; }   
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+    }
+}
---
+++ NzWalksAPI/NzWalksAPI/Models/Domain/Walk.cs
@@ -0,0 +1,23 @@
+﻿namespace NzWalksAPI.Models.Domain
+{
+    public class Walk
+    {
+
+        public Guid Id  { get; set; }
+
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+
+        public Guid RegionID { get; set; }
+
+        public Guid WalkDifficultyId { get; set; }
+
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+
+
+    }
+}
